11e850da4c04898a872a80abc8c524ad9b367146
Yara Prins <yaraprins@live.nl>
2022-02-10 16:16:34 +0100

    added Matching App folder in BackEnd

---
+++ Back-End-Development/Matching App/helloworld.js
@@ -0,0 +1,14 @@
+const name = "Yara";
+const name2 = "Bob";
+let age = "24";
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send(`Hello ${name}, How are you? I'm ${name2}! I'm ${age} years old.`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

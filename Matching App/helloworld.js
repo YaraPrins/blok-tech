fd82a4dd63062dc81c04b090c553a4d90b77eca6
Yara Prins <yaraprins@live.nl>
2022-02-18 19:46:24 +0100

    wat nettere code

--- Matching App/helloworld.js
+++ Matching App/helloworld.js
@@ -1,9 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 const name = "Yara";
 const name2 = "Bob";
 let age = "24";
-const express = require('express')
-const app = express()
-const port = 5000
 
 app.get('/', (req, res) => {
   res.send(`Hello ${name}, How are you? I'm ${name2}! I'm ${age} years old.`)

8c2c1f8cf1ecc7428dc9013e317f5a721d809a27
Yara Prins <yaraprins@live.nl>
2022-02-18 21:29:50 +0100

    added 404 page, routes, starts with nodemon

--- Matching App/server.js
+++ Matching App/server.js
@@ -2,10 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/profile/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`)
+})
+app.get('/about', (req, res) => {
+  res.send('This is an About page')
+})
+app.get('/login', (req, res) => {
+  res.send('Please login to continue!')
+})
+app.get('/:name/liked-songs', (req, res) => {
+  res.send(`Hello ${req.params.name}, these are your liked songs!`)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+
+// ========================
+// ========================
+// STATUS 500
+// ========================
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+// ========================
+// ========================
+// ERROR 404
+// ========================
+app.use((req, res, next) => {
+  res.status(404).send("Error 404 (Sorry! Page not found)")
 })

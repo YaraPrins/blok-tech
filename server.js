74cdb54ffecf17f5093a67c2abb3ced0eed86f1a
Yara Prins <yaraprins@live.nl>
2022-03-06 18:50:25 +0100

    tried to install HBS, cant make it show the dynamic pages though...

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
-// ========================
-// ========================
-// STATIC FILES
-// ========================
-app.use(express.static('public'))
+// // ========================
+// // ========================
+// // STATIC FILES
+// // ========================
+app.use(express.static('public'));
 
 
-// ========================
-// ========================
-// TEMPLATING ENGINE
-// ========================
+// // ========================
+// // ========================
+// // TEMPLATING ENGINE
+// // ========================
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-
-// const Handlebars = require("handlebars");
-// const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Yara" }));
+const Handlebars = require('handlebars');
+const template = Handlebars.compile('Name: {{name}}');
+console.log(template({ name: 'Yara' }));
 
 
 // ========================
@@ -26,26 +27,34 @@ app.use(express.static('public'))
 // ROUTES
 // ========================
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('home')
+});
+
 app.get('/profile/:name', (req, res) => {
   res.send(`Hello ${req.params.name}`)
-})
+});
+
 app.get('/about', (req, res) => {
   res.send('This is an About page')
-})
+});
+
 app.get('/login', (req, res) => {
   res.send('Please login to continue!')
-})
+});
+
 app.get('/:name/liked-songs', (req, res) => {
   res.send(`Hello ${req.params.name}, these are your liked songs!`)
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+});
 
 
+
+// // ========================
+// // ========================
+// // ERROR 404
+// // ========================
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry! Can\'t find that!')
+});
 
 // ========================
 // ========================
@@ -56,10 +65,8 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!')
 })
 
-// ========================
-// ========================
-// ERROR 404
-// ========================
-app.use((req, res, next) => {
-  res.status(404).send("Error 404 (Sorry! Page not found)")
-})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});

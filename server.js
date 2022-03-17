27ad93779db0c45a0a9d54cfc1afb688faf80742
Yara Prins <yaraprins@live.nl>
2022-03-17 12:27:22 +0100

    fout in code verholpen 'app.use funtionn setHeader content type text plain'

--- server.js
+++ server.js
@@ -83,11 +83,11 @@ app.use(bodyParser.urlencoded({extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function(req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+// app.use(function(req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
 
 
 // ========================
